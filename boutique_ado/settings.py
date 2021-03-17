13d6c500bb0803fc204f89d20a91708ecbbf7597
Behnaz Khoshnood <behnaz.khoshnood@gmail.com>
2021-03-17 13:41:23 +0000

    install stripe, adding the secret keys

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
+SECRET_KEY = '=r124%bg*#po3f!%k)#1k_l)&9qqbb7g0d8d!1xg*ql%nerz!)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -164,5 +164,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# stripe
 FREE_DELIVERY_THRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
