02ac5ef9d6a266ebc1c1fa377b591ef4c8dd3960
Behnaz Khoshnood <behnaz.khoshnood@gmail.com>
2021-04-08 10:32:58 +0000

    working on error 500 for payment-intent-success

--- bag/views.py
+++ bag/views.py
@@ -28,7 +28,7 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
-                messages.success(request, f'Uppdated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
                 messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
@@ -38,7 +38,7 @@ def add_to_bag(request, item_id):
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
-            messages.success(request, f'Uppdated {product.name} quantity to {bag[item_id]}')
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
@@ -60,7 +60,7 @@ def adjust_bag(request, item_id):
     if size:
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
-            messages.success(request, f'Uppdated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+            messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
@@ -69,7 +69,7 @@ def adjust_bag(request, item_id):
     else:
         if quantity > 0:
             bag[item_id] = quantity
-            messages.success(request, f'Uppdated {product.name} quantity to {bag[item_id]}')
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag.pop(item_id)
             messages.success(request, f'Removed {product.name} from your bag')
--- checkout/migrations/0001_initial.py
+++ checkout/migrations/0001_initial.py
@@ -31,8 +31,6 @@ class Migration(migrations.Migration):
                 ('delivery_cost', models.DecimalField(decimal_places=2, default=0, max_digits=6)),
                 ('order_total', models.DecimalField(decimal_places=2, default=0, max_digits=10)),
                 ('grand_total', models.DecimalField(decimal_places=2, default=0, max_digits=10)),
-                ('original_bag', models.TextField(default='')),
-                ('stripe_pid', models.CharField(default='', max_length=254)),
             ],
         ),
         migrations.CreateModel(
@@ -42,8 +40,8 @@ class Migration(migrations.Migration):
                 ('product_size', models.CharField(blank=True, max_length=2, null=True)),
                 ('quantity', models.IntegerField(default=0)),
                 ('lineitem_total', models.DecimalField(decimal_places=2, editable=False, max_digits=6)),
-                ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='lineitems', to='checkout.order')),
-                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='products.product')),
+                ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='lineitems', to='checkout.Order')),
+                ('product', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='products.Product')),
             ],
         ),
     ]
